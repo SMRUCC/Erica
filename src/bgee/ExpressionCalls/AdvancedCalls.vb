5bd8f9d8e2f1f9d1c6f453eea7facf4c311f79fe
xieguigang <xie.guigang@gcmodeller.org>
2021-12-05 02:06:39 +0800

    read table code

---
+++ src/bgee/ExpressionCalls/AdvancedCalls.vb
@@ -0,0 +1,82 @@
+﻿Imports Microsoft.VisualBasic.Text
+
+Public Class AdvancedCalls
+
+    Public Property geneID As String
+    Public Property gene_name As String
+    Public Property anatomicalID As String
+    Public Property anatomicalName As String
+    Public Property developmental_stageID As String
+    Public Property developmental_stage As String
+    Public Property expression As String
+    Public Property call_quality As String
+    Public Property expression_rank As String
+    Public Property including_observed_data As String
+    Public Property affymetrix As GeneExpression
+    Public Property EST_data As GeneExpression
+    Public Property In_Situ As GeneExpression
+    Public Property RNASeq As GeneExpression
+
+    Public Shared Iterator Function ParseTable(file As String) As IEnumerable(Of AdvancedCalls)
+        For Each line As String In file.LineIterators.Skip(1)
+            Yield ParseLine(line.Split(ASCII.TAB))
+        Next
+    End Function
+
+    Private Shared Function ParseLine(tsv As String()) As AdvancedCalls
+        Return New AdvancedCalls With {
+            .geneID = tsv(0),
+            .gene_name = tsv(1),
+            .anatomicalID = tsv(2),
+            .anatomicalName = tsv(3),
+            .developmental_stageID = tsv(4),
+            .developmental_stage = tsv(5),
+            .expression = tsv(6),
+            .call_quality = tsv(7),
+            .expression_rank = tsv(8),
+            .including_observed_data = tsv(9),
+            .affymetrix = New GeneExpression With {
+                .data = tsv(10),
+                .expression_high_quality = Integer.Parse(tsv(11)),
+                .expression_low_quality = Integer.Parse(tsv(12)),
+                .absence_high_quality = Integer.Parse(tsv(13)),
+                .absence_low_quality = Integer.Parse(tsv(14)),
+                .observed_data = tsv(15)
+            },
+            .EST_data = New GeneExpression With {
+                .data = tsv(16),
+                .expression_high_quality = Integer.Parse(tsv(17)),
+                .expression_low_quality = Integer.Parse(tsv(18)),
+                .observed_data = tsv(19)
+            },
+            .In_Situ = New GeneExpression With {
+                .data = tsv(20),
+                .expression_high_quality = Integer.Parse(tsv(21)),
+                .expression_low_quality = Integer.Parse(tsv(22)),
+                .absence_high_quality = Integer.Parse(tsv(23)),
+                .absence_low_quality = Integer.Parse(tsv(24)),
+                .observed_data = tsv(25)
+            },
+            .RNASeq = New GeneExpression With {
+                .data = tsv(26),
+                .expression_high_quality = Integer.Parse(tsv(27)),
+                .expression_low_quality = Integer.Parse(tsv(28)),
+                .absence_high_quality = Integer.Parse(tsv(29)),
+                .absence_low_quality = Integer.Parse(tsv(30)),
+                .observed_data = tsv(31)
+            }
+        }
+    End Function
+
+End Class
+
+Public Class GeneExpression
+
+    Public Property data As String
+    Public Property expression_high_quality As Integer
+    Public Property expression_low_quality As Integer
+    Public Property absence_high_quality As Integer
+    Public Property absence_low_quality As Integer
+    Public Property observed_data As String
+
+End Class
--- test/test/Program.vb
+++ test/test/Program.vb
@@ -6,6 +6,7 @@ Module Program
     Sub Main(args As String())
 
         Dim data = "E:\Erica\test\bgee.json".LoadJSON(Of DataSet)
+        Dim calls = AdvancedCalls.ParseTable("P:\Bgee\Mus_musculus_expr_advanced_development.tsv").Take(10).ToArray
 
         Pause()
     End Sub
