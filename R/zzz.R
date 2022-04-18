373386aa307a366faa89479b106912f088094b8b
xieguigang <xie.guigang@gcmodeller.org>
2022-04-18 12:23:31 +0800

    save result background

--- R/zzz.R
+++ R/zzz.R
@@ -2,6 +2,7 @@ require(GCModeller);
 
 imports ["phenograph", "singleCell", "Bgee"] from "Erica";
 imports "geneExpression" from "phenotype_kit";
+imports "background" from "gseakit";
 
 const .onLoad = function() {
 	
--- test/bgee/human_tissues.R
+++ test/bgee/human_tissues.R
@@ -1,5 +1,8 @@
 require(Erica);
 
-bgee = Bgee::parseTsv("F:\human\Homo_sapiens_expr_simple_all_conditions.tsv");
+setwd(@dir);
+
+bgee = Bgee::parseTsv("./Homo_sapiens_expr_simple_all_conditions.tsv");
 background = Bgee::tissue_background(bgee);
 
+write.background(background, file = "./human_tissues.XML");
