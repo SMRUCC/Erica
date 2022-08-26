1f99a02b674ab358b0c8ef143aa0cde214d15e98
guigang xie <xie.guigang@gcmodeller.org>
2022-08-26 14:21:39 +0800

    add test for read data

--- src/STRaid/ReadData.vb
+++ src/STRaid/ReadData.vb
@@ -1,16 +1,10 @@
-Imports Microsoft.VisualBasic.Data.IO.HDF5
+Imports HDF.PInvoke
 
 Public Class ReadData
 
-    Dim buffer As HDF5File
 
     Sub New(path As String)
-        buffer = HDF5File.Open(path)
-        Dim genome = buffer("/var/__categories/genome").data
-
-        Dim feature_types = buffer("/var/__categories/feature_types").data
-
-        Dim X = buffer("/X/data").data
+        Dim fileId = H5F.open(path, H5F.ACC_RDONLY)
 
         Pause()
     End Sub
