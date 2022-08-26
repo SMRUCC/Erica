a42f71e9b0cfaca95a0a305e654a711f2ef1ac49
guigang xie <xie.guigang@gcmodeller.org>
2022-08-26 16:36:00 +0800

    how to parse the X data matrix

--- src/STRaid/Slots/X.vb
+++ src/STRaid/Slots/X.vb
@@ -3,7 +3,18 @@
 ''' </summary>
 Public Class X
 
+    ''' <summary>
+    ''' 构架出一个稀疏矩阵
+    ''' </summary>
+    ''' <param name="xdata"></param>
+    ''' <param name="xindices"></param>
+    ''' <param name="xindptr"></param>
+    ''' <returns></returns>
     Friend Shared Function ShapeMatrix(xdata As Single(), xindices As Integer(), xindptr As Integer()) As X
+        Dim left As Integer = Scan0
 
+        For Each idx As Integer In xindptr.Skip(1)
+
+        Next
     End Function
 End Class
