45ce7562f4d59cf45c7cbcdd9ca9528e2f37bbf5
guigang xie <xie.guigang@gcmodeller.org>
2022-08-26 18:01:19 +0800

    Create loadUmap.R

---
+++ test/h5ad/loadUmap.R
@@ -0,0 +1,24 @@
+require(Erica);
+
+imports "singleCells" from "Erica";
+
+umap = "F:\ST202208234793930\raw\10x_Visium_deal.h5ad"
+|> read.h5ad()
+|> umap_annotations()
+;
+
+write.csv(umap, file = `${@dir}/umap.csv`, row.names = FALSE);
+
+bitmap(file = `${@dir}/umap.png`) {
+	plot(umap[, "x"], umap[, "y"],
+		 padding      = "padding:200px 400px 200px 250px;",
+		 class        = umap[, "class"],
+		 title        = "UMAP 2D Scatter",
+		 x.lab        = "dimension 1",
+		 y.lab        = "dimension 2",
+		 legend.block = 13,
+		 colorSet     = "paper", 
+		 grid.fill    = "transparent",
+		 size         = [2600, 1600]
+	);
+}
