6d10e3d943355f62840b0834d1e703cc38fdfc55
xieguigang <xie.guigang@gcmodeller.org>
2022-08-26 23:56:01 +0800

    get render layer

---
+++ src/Erica/stImaging.vb
@@ -0,0 +1,13 @@
+﻿
+Imports Microsoft.VisualBasic.MIME.Html
+Imports Microsoft.VisualBasic.Scripting.MetaData
+Imports STRaid
+
+<Package("st-imaging")>
+Module stImaging
+
+    <exportapi("new_render")>
+    Public Function createRender(h5ad As AnnData) As Render
+        Return New Render(h5ad)
+    End Function
+End Module
---
+++ src/STImaging/Render.vb
@@ -0,0 +1,42 @@
+Imports System.Drawing
+Imports Microsoft.VisualBasic.Imaging.Drawing2D.HeatMap
+Imports SMRUCC.genomics.Analysis.HTS.DataFrame
+Imports STRaid
+
+Public Class Render
+
+    Dim matrix As Dictionary(Of String, DataFrameRow)
+    Dim pixels As Point()
+
+    Public ReadOnly Property geneIds As String()
+        Get
+            Return matrix.Keys.ToArray
+        End Get
+    End Property
+
+    Sub New(anndat As AnnData)
+        Dim matrix = anndat.ExportExpression.T
+
+        Me.matrix = matrix.expression.ToDictionary(Function(a) a.geneID)
+        Me.pixels = matrix.sampleID _
+            .Select(Function(str)
+                        Dim t As Integer() = str _
+                            .Split(","c) _
+                            .Select(AddressOf Integer.Parse) _
+                            .ToArray
+                        Dim p As New Point(t(0), t(1))
+
+                        Return p
+                    End Function) _
+            .ToArray
+    End Sub
+
+    Public Iterator Function GetLayer(geneId As String) As IEnumerable(Of PixelData)
+        Dim vec As DataFrameRow = matrix(geneId)
+
+        For i As Integer = 0 To pixels.Length - 1
+            Yield New PixelData(pixels(i), vec.experiments(i))
+        Next
+    End Function
+
+End Class
