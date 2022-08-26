8235de95f8ee947c266db492f4e7bff24bf32e1f
guigang xie <xie.guigang@gcmodeller.org>
2022-08-26 14:27:26 +0800

    Update ReadData.vb

--- src/STRaid/ReadData.vb
+++ src/STRaid/ReadData.vb
@@ -5,6 +5,8 @@ Public Class ReadData
 
     Sub New(path As String)
         Dim fileId = H5F.open(path, H5F.ACC_RDONLY)
+        Dim dataSetId = H5D.open(fileId, "/X/data")
+        Dim dataSpaceId = H5D.get_space(dataSetId)
 
         Pause()
     End Sub
