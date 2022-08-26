a237e40a76c5f50e66cf45004506777a44a5c913
xieguigang <xie.guigang@gcmodeller.org>
2022-08-27 02:11:20 +0800

    Update rendering.R

--- test/h5ad/rendering.R
+++ test/h5ad/rendering.R
@@ -1,22 +1,25 @@
-require(Erica);
 require(GCModeller);
+require(Erica);
 
 imports "singleCell" from "Erica";
 imports "st-imaging" from "Erica";
 imports "geneExpression" from "phenotype_kit";
 
-raw = "K:\ST\10x_Visium_deal.h5ad"
+img = "K:\\ST\\10x_Visium_deal.h5ad"
 |> read.h5ad()
+|> new_render(raw)
 ;
 
-img = new_render(raw);
-
 print([img]::geneIDs);
+
+
 
 for(id in ["ENSMUSG00000103810", "ENSMUSG00000047459",
 "ENSMUSG00000029581","ENSMUSG00000066640",
 "ENSMUSG00000045467"]) {
 # id = "ENSMUSG00000047459";
+
+cat("\n\n");
 
 bitmap(file = `${@dir}/${id}.png`) {
     imaging(img, id);
