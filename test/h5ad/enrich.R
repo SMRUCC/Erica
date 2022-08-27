efda718d9e6dbb897c8305064baf896dc95e25a7
xieguigang <xie.guigang@gcmodeller.org>
2022-08-28 04:07:15 +0800

    plot enrichment

--- test/h5ad/enrich.R
+++ test/h5ad/enrich.R
@@ -4,10 +4,14 @@ require(Erica);
 imports "singleCell" from "Erica";
 imports "background" from "gseakit";
 imports "GSEA" from "gseakit";
+imports "UniProt" from "gseakit";
+imports "visualPlot" from "visualkit";
 
 raw = "K:\ST\10x_Visium_deal.h5ad"
 |> read.h5ad()
 ;
+
+keywords_list = UniProtKeywords();
 
 geneList = expression_list(raw, q = 0.5);
 
@@ -19,9 +23,20 @@ keywords = read.background("E:\\Erica\\test\\background\\uniprot_keywords.xml");
 for(key in names(geneList)) {
     genes =  geneList[[key]];
     enrich = keywords |> enrichment(genes, outputAll = FALSE);
+    profiles = keyword_profiles(enrich,keywords_list, top = 3);
     enrich = as.data.frame(enrich);
 
     # print(enrich, max.print = 13);
+    bitmap(file = `${@dir}/keywords/${key}.png`) {
+        category_profiles.plot(
+            profiles ,
+             colors = "paper",
+             title = "UniProt Keywords",
+             axis.title = "-log10(p-value)",
+             size = [1200,1600],
+             dpi = 100
+             );
+    }
 
     write.csv(enrich, file = `${@dir}/keywords/${key}.csv`);
 }
