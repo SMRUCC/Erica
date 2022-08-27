8010d6fa6cc208d1c5bd6c88be74208f1f905195
xieguigang <xie.guigang@gcmodeller.org>
2022-08-27 18:13:09 +0800

    do enrichment test

--- test/h5ad/enrich.R
+++ test/h5ad/enrich.R
@@ -1,6 +1,9 @@
+require(GCModeller);
 require(Erica);
 
 imports "singleCell" from "Erica";
+imports "background" from "gseakit";
+imports "GSEA" from "gseakit";
 
 raw = "K:\ST\10x_Visium_deal.h5ad"
 |> read.h5ad()
@@ -9,3 +12,14 @@ raw = "K:\ST\10x_Visium_deal.h5ad"
 geneList = expression_list(raw);
 
 str(geneList);
+print(names(geneList));
+
+keywords = read.background("E:\\Erica\\test\\background\\uniprot_keywords.xml");
+
+for(key in names(geneList)) {
+    genes =  geneList[[key]];
+    enrich = keywords |> enrichment(genes, outputAll = FALSE);
+    enrich = as.data.frame(enrich);
+
+    print(enrich, max.print = 13);
+}
