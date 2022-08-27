f1e20035ab80527218574df493346d86658c4918
xieguigang <xie.guigang@gcmodeller.org>
2022-08-28 02:07:48 +0800

    Create enrich_bgee.R

---
+++ test/h5ad/enrich_bgee.R
@@ -0,0 +1,27 @@
+require(GCModeller);
+require(Erica);
+
+imports "singleCell" from "Erica";
+imports "Bgee" from "Erica";
+imports "background" from "gseakit";
+imports "GSEA" from "gseakit";
+
+raw = "K:\ST\10x_Visium_deal.h5ad"
+|> read.h5ad()
+;
+
+geneList = expression_list(raw, q = 0.5);
+bgee = read.backgroundPack()
+
+str(geneList);
+print(names(geneList));
+
+for(key in names(geneList)) {
+    genes =  geneList[[key]];
+    enrich = keywords |> enrichment(genes, outputAll = FALSE);
+    enrich = as.data.frame(enrich);
+
+    # print(enrich, max.print = 13);
+
+    write.csv(enrich, file = `${@dir}/bgee/${key}.csv`);
+}
