9d0b58c3f24ede24a793ebb2ced4c4d95ece5e71
xieguigang <xie.guigang@gcmodeller.org>
2022-08-28 00:03:08 +0800

    binary datawriter

---
+++ src/bgee/BgeeDisk/BgeeDiskWriter.vb
@@ -0,0 +1,38 @@
+﻿Imports Microsoft.VisualBasic.DataStorage.HDSPack.FileSystem
+
+Public Class BgeeDiskWriter : Implements IDisposable
+
+    ReadOnly disk As StreamPack
+
+    Private disposedValue As Boolean
+
+    Sub New(file As String)
+        disk = StreamPack.CreateNewStream(file, init_size:=4096 * 1024)
+    End Sub
+
+    Protected Overridable Sub Dispose(disposing As Boolean)
+        If Not disposedValue Then
+            If disposing Then
+                ' TODO: dispose managed state (managed objects)
+                Call disk.Dispose()
+            End If
+
+            ' TODO: free unmanaged resources (unmanaged objects) and override finalizer
+            ' TODO: set large fields to null
+            disposedValue = True
+        End If
+    End Sub
+
+    ' ' TODO: override finalizer only if 'Dispose(disposing As Boolean)' has code to free unmanaged resources
+    ' Protected Overrides Sub Finalize()
+    '     ' Do not change this code. Put cleanup code in 'Dispose(disposing As Boolean)' method
+    '     Dispose(disposing:=False)
+    '     MyBase.Finalize()
+    ' End Sub
+
+    Public Sub Dispose() Implements IDisposable.Dispose
+        ' Do not change this code. Put cleanup code in 'Dispose(disposing As Boolean)' method
+        Dispose(disposing:=True)
+        GC.SuppressFinalize(Me)
+    End Sub
+End Class
