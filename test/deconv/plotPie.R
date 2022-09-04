62fe482b0b481c9508f0a43beacb2912477d3de8
xieguigang <xie.guigang@gcmodeller.org>
2022-09-04 10:41:33 +0800

    Update plotPie.R

--- test/deconv/plotPie.R
+++ test/deconv/plotPie.R
@@ -14,4 +14,17 @@ xy = lapply(xy, a -> as.numeric(a));
 
 str(xy);
 
+data[, "x"] = sapply(xy, a -> a[1]);
+data[, "y"] = sapply(xy, a -> a[2]);
+
 print(data, max.print = 13);
+
+bitmap(file = `${@dir}/stdeconv_scatterpie.png`) {
+
+	ggplot(data, aes(x = "x", y = "y"))
+	+ geom_scatterpie(data = [
+	"topic_1","topic_2","topic_3","topic_4","topic_5",
+	"topic_6","topic_7","topic_8","topic_9","topic_10"
+	]);
+
+}
