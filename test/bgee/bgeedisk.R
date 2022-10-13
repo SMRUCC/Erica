26c1d3c453232921428626114c175c7bc83485b4
xieguigang <xie.guigang@gcmodeller.org>
2022-10-13 09:56:02 +0800

    run data imports

--- test/bgee/bgeedisk.R
+++ test/bgee/bgeedisk.R
@@ -1,6 +1,8 @@
 require(GCModeller);
 require(Erica);
 
-bgee = Bgee::parseTsv("K:\\ST\\Mus_musculus_expr_simple_all_conditions.tsv",pip.stream = TRUE);
+setwd(@dir);
 
-write.backgroundPack(bgee, file = `${@dir}/mmu.bgee`);
+bgee = Bgee::parseTsv("./Homo_sapiens_expr_simple_all_conditions.tsv",pip.stream = TRUE);
+
+write.backgroundPack(bgee, file = "./hsa.bgee");
