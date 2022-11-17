cde017d1aff288f156ab2b44c14ba1e7ae21ce9f
guigang xie <xie.guigang@gcmodeller.org>
2022-11-17 15:13:28 +0800

    Create loadTest.R

---
+++ test/ST_spaceranger/loadTest.R
@@ -0,0 +1,10 @@
+imports "package_utils" from "devkit";
+
+package_utils::attach("D:\Erica");
+
+
+imports "STdata" from "Erica";
+
+matrix = read.ST_spacerangerH5Matrix("F:\20221117-ST\raw_feature_bc_matrix.h5");
+spots = read.spatial_spots("F:\20221117-ST\tissue_positions_list.csv");
+
