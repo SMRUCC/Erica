fa6bdde35a018c5ee856d9e27112366f228a3148
xieguigang <gg.xie@bionovogene.com>
2022-12-29 08:54:59 +0800

    Update SpotHeatMap.vb

--- src/STImaging/10X/SpotHeatMap.vb
+++ src/STImaging/10X/SpotHeatMap.vb
@@ -5,15 +5,16 @@ Imports Microsoft.VisualBasic.Imaging
 Imports Microsoft.VisualBasic.Imaging.Drawing2D
 Imports Microsoft.VisualBasic.Imaging.Drawing2D.HeatMap
 
-Public Class SpotHeatMap : Inherits Plot
+Public Class SpotHeatMap : Inherits HeatMapPlot
 
     ReadOnly spots As PixelData()
     ReadOnly dimension_size As Size
 
-    Public Property mapLevels As Integer = 16
-
     Public Sub New(layer As PixelData(), dimension_size As Size, theme As Theme)
         MyBase.New(theme)
+
+        Me.spots = layer
+        Me.dimension_size = dimension_size
     End Sub
 
     Protected Overrides Sub PlotInternal(ByRef g As IGraphics, canvas As GraphicsRegion)
