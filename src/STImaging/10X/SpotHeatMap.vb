534b21667fe6e5b2f7787bc98a0345f2413536f4
xieguigang <gg.xie@bionovogene.com>
2022-12-29 08:49:34 +0800

    Create SpotHeatMap.vb

---
+++ src/STImaging/10X/SpotHeatMap.vb
@@ -0,0 +1,22 @@
+﻿Imports System.Drawing
+Imports Microsoft.VisualBasic.Data.ChartPlots.Graphic
+Imports Microsoft.VisualBasic.Data.ChartPlots.Graphic.Canvas
+Imports Microsoft.VisualBasic.Imaging
+Imports Microsoft.VisualBasic.Imaging.Drawing2D
+Imports Microsoft.VisualBasic.Imaging.Drawing2D.HeatMap
+
+Public Class SpotHeatMap : Inherits Plot
+
+    ReadOnly spots As PixelData()
+    ReadOnly dimension_size As Size
+
+    Public Property mapLevels As Integer = 16
+
+    Public Sub New(layer As PixelData(), dimension_size As Size, theme As Theme)
+        MyBase.New(theme)
+    End Sub
+
+    Protected Overrides Sub PlotInternal(ByRef g As IGraphics, canvas As GraphicsRegion)
+
+    End Sub
+End Class
