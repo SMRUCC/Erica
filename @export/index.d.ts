5bbb709984f1d860b06761434f59ab5fda4183c0
xieguigang <xie.guigang@gcmodeller.org>
2023-05-08 11:46:03 +0800

    Create index.d.ts

---
+++ @export/index.d.ts
@@ -0,0 +1,14 @@
+﻿// export R# source type define for javascript/typescript language
+//
+// package_source=Erica
+
+declare namespace Erica {
+   /**
+   */
+   function UniProtKeywords(): object;
+   module  {
+      /**
+      */
+      function onLoad(): object;
+   }
+}
