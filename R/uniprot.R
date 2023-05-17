75ee7fbff6d38e0355a8343c98a7d79854a12f59
xieguigang <xie.guigang@gcmodeller.org>
2023-05-18 01:14:43 +0800

    add human gene expression json data

--- R/uniprot.R
+++ R/uniprot.R
@@ -12,12 +12,17 @@ const human_genes.annotations = function(index = "Bgee") {
     let geneIDs = unique(df[, index]);
     let i = geneIDs != "";
 
+    print("removes missing genes...");
     geneIDs = geneIDs[i];
     df = df[i, ];
+
+    print(`get ${nrow(df)} which has gene id assigned!`);
 
     let annos = as.list(df, byrow = TRUE);
     let annoData = list();
     
+    print("create gene annotation json...");
+
     for(gene in annos) {
         geneIDs = strsplit(gene[[index]], ";");
         gene$"Gene Names (synonym)" = strsplit(gene$"Gene Names (synonym)");
