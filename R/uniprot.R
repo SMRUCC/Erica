97c08f8dae0b5abf01e63435c7b8669d78163ab0
xieguigang <xie.guigang@gcmodeller.org>
2023-05-18 01:09:55 +0800

    export gene annotations

--- R/uniprot.R
+++ R/uniprot.R
@@ -1,10 +1,31 @@
 #' Load human genes for run single cells for ST data annotation
 #'
 const load.human_genes = function() {
-    const df = "data/HUMAN.tsv"
+    "data/HUMAN.tsv"
     |> system.file(package = "Erica")
     |> read.csv(row.names = NULL, check.names = FALSE, tsv = TRUE)
     ;
+}
 
-    str(df);
+const human_genes.annotations = function(index = "Bgee") {
+    let df = load.human_genes();
+    let geneIDs = unique(df[, index]);
+    let i = geneIDs != "";
+
+    geneIDs = geneIDs[i];
+    df = df[i, ];
+
+    let annos = as.list(df, byrow = TRUE);
+    let annoData = list();
+    
+    for(gene in annos) {
+        geneIDs = strsplit(gene[[index]], ";");
+        gene$"Gene Names (synonym)" = strsplit(gene$"Gene Names (synonym)");
+
+        for(id in geneIDs) {
+            annoData[[id]] = gene;
+        }
+    }
+
+    annoData;
 }
