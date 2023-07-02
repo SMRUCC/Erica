6d8d632b8993fcb691868b2004daf1c9617d6c0c
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-07-03 00:12:14 +0800

    convert test cli to cli tool

--- src/SingleCell/STdeconvolve/test/Program.vb
+++ src/SingleCell/STdeconvolve/test/Program.vb
@@ -1,33 +1,27 @@
+Imports Microsoft.VisualBasic.CommandLine
 Imports Microsoft.VisualBasic.Serialization.JSON
 Imports SMRUCC.genomics.Analysis.HTS.DataFrame
 Imports SMRUCC.genomics.Analysis.SingleCell.STdeconvolve
 
 Module Program
 
-    Const deconv_out = "\Erica\src\SingleCell\STdeconvolve\demo\HR2MSI mouse urinary bladder S096_top3.json"
+    Public Function Main(args As String()) As Integer
+        Return GetType(Program).RunCLI(App.CommandLine, executeFile:=AddressOf runOnMatrix)
+    End Function
 
-    Sub Main(args As String())
-        Call dumpMatrix()
-        Call createDeconv()
-    End Sub
-
-    Sub createDeconv()
-        Dim STdataset As Matrix = Matrix.LoadData("\Erica\src\SingleCell\demo\HR2MSI mouse urinary bladder S096_top3.csv")
+    Private Function runOnMatrix(file As String, args As CommandLine) As Integer
+        Dim exports_json As String = file.TrimSuffix & "-STdeconvolve.json"
+        Dim deconv_csv As String = file.TrimSuffix & "-STdeconvolve.csv"
+        Dim deconv_layers As String = file.TrimSuffix & "-cell_layers.csv"
+        Dim STdataset As Matrix = Matrix.LoadData(file)
         Dim corpus As STCorpus = STdataset.CreateSpatialDocuments
         Dim result = corpus.LDAModelling(4, iterations:=120).Deconvolve(corpus, topGenes:=300)
+        Dim deconv = result.GetSingleCellExpressionMatrix(STdataset)
 
-        Call result.GetJson.SaveTo(deconv_out)
+        Call result.GetJson.SaveTo(exports_json)
+        Call deconv.SaveMatrix(deconv_csv, "spot_xy")
+        Call result.theta.SaveMatrix(deconv_layers)
 
-        Pause()
-    End Sub
-
-    Sub dumpMatrix()
-        Dim STdataset As Matrix = Matrix.LoadData("\Erica\src\SingleCell\demo\HR2MSI mouse urinary bladder S096_top3.csv")
-        Dim data = deconv_out.LoadJSON(Of Deconvolve)
-        Dim deconv = data.GetSingleCellExpressionMatrix(STdataset)
-
-        Call deconv.SaveMatrix("\Erica\src\SingleCell\STdeconvolve\demo\HR2MSI mouse urinary bladder S096-deconv.csv", "spot_xy")
-        Call data.theta.SaveMatrix(deconv_out.ChangeSuffix("csv"))
-        Call Pause()
-    End Sub
+        Return 0
+    End Function
 End Module
