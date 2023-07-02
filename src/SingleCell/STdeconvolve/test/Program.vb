8b9d16cade4f9b2ab4ad35b0d76c409953701c0a
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-07-02 22:36:13 +0800

    fix for the conditional compiling

--- src/SingleCell/STdeconvolve/test/Program.vb
+++ src/SingleCell/STdeconvolve/test/Program.vb
@@ -5,10 +5,10 @@ Imports SMRUCC.genomics.Analysis.SingleCell.STdeconvolve
 
 Module Program
 
-    Const deconv_out = "G:\Erica\src\SingleCell\STdeconvolve\demo\HR2MSI mouse urinary bladder S096_top3.json"
+    Const deconv_out = "\Erica\src\SingleCell\STdeconvolve\demo\HR2MSI mouse urinary bladder S096_top3.json"
 
     Sub Main(args As String())
-        Dim STdataset As Matrix = Matrix.LoadData("G:\Erica\src\SingleCell\demo\HR2MSI mouse urinary bladder S096_top3.csv")
+        Dim STdataset As Matrix = Matrix.LoadData("\Erica\src\SingleCell\demo\HR2MSI mouse urinary bladder S096_top3.csv")
         Dim corpus As STCorpus = STdataset.CreateSpatialDocuments
         Dim result = corpus.LDAModelling(13).Deconvolve(corpus)
 
