6a5657128d1c52d11f1ff10ae3f69e844b900f2c
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-07-09 20:59:14 +0800

    make improvements of the cluster alignment score helper

---
+++ src/SingleCell/PhenoGraph/PhenoGraph/SpatialGraph.vb
@@ -0,0 +1,13 @@
+﻿Imports Microsoft.VisualBasic.DataMining.BinaryTree
+Imports SMRUCC.genomics.Analysis.HTS.DataFrame
+
+''' <summary>
+''' Build a spatial graph via the grap clustering
+''' </summary>
+Public Module SpatialGraph
+
+    Public Function CreateGraph(x As Matrix) As ClusterTree
+        Dim tree As New ClusterTree
+
+    End Function
+End Module
