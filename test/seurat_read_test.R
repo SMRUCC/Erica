d4bf7806060f364bc8f28c0ae942073fac4ae8ed
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-06-04 22:11:20 +0800

    Update seurat_read_test.R

--- test/seurat_read_test.R
+++ test/seurat_read_test.R
@@ -4,5 +4,10 @@ imports "STdata" from "Erica";
 
 setwd(@dir);
 
+let r = readRData("./SCA_CyTOF_TISSUE_WHOLE_BLOOD_SDY1389_Whole_Blood.RDS");
+
+str(r);
+stop();
+
 let m = read.seurat("./SCA_CyTOF_TISSUE_WHOLE_BLOOD_SDY1389_Whole_Blood.RDS");
 
