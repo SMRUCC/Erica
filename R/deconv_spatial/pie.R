c2c8b8c85d7f02693c4753a6c8f82a5e49104681
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-06-20 16:51:49 +0800

    make improvements of the plot function

--- R/deconv_spatial/pie.R
+++ R/deconv_spatial/pie.R
@@ -2,23 +2,35 @@ require(ggplot);
 
 #' Plot the scatter pie data
 #' 
-#' Display the cell distribution
+#' @param cell_layers the cell class probablity matrix data
+#' @param x the x axis for draw a pie, could be x scan location of the spatial data or x dimension of the single cells umap embedding result.
+#' @param y the y axis for draw a pie, could be y scan location of the spatial data or y dimension of the single cells umap embedding result.
 #' 
-const plot_pie = function(cell_layers) {
+#' @details Display the cell/spot class distribution
+#' 
+const plot_pie = function(cell_layers, x = NULL, y = NULL) {
     let data = as.data.frame(cell_layers);
-    let spatial = rownames(data);
-    let xy = strsplit(spatial, ",");
     let class_labels = colnames(data);
 
-    names(xy) = spatial;
-    xy = lapply(xy, a -> as.numeric(a));
+    if (isTRUE((length(x) == 0) || (length(y) == 0))) {
+        # needs parsed from the spatial labels
+        let spatial = rownames(data);
+        let xy = strsplit(spatial, ",");
+        
+        names(xy) = spatial;
+        xy = lapply(xy, a -> as.numeric(a));
 
-    print("get spatial [x,y] information:");
-    str(xy);
+        print("get spatial [x,y] information:");
+        str(xy);
 
-    data[, "x"] = sapply(xy, a -> a[1]);
-    data[, "y"] = sapply(xy, a -> a[2]);
+        data[, "x"] = xy@{1};
+        data[, "y"] = xy@{2};
+    } else {
+        data[, "x"] = as.numeric(x);
+        data[, "y"] = as.numeric(y);
+    }    
 
+    print("view of the scatter pie input data that tagged with the x,y axis data:");
     print(data, max.print = 13);
 
     ggplot(data, aes(x = "x", y = "y"), 
