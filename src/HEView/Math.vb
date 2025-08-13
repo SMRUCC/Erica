91d2392a38b23dbefec3aed0ce769b33178f287c
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-08-13 11:35:47 +0800

    fill moran-I value to cells

--- src/HEView/Math.vb
+++ src/HEView/Math.vb
@@ -1,9 +1,13 @@
-﻿Imports Microsoft.VisualBasic.Data.GraphTheory.GridGraph
+﻿Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.Data.GraphTheory.GridGraph
+Imports Microsoft.VisualBasic.Imaging
 Imports Microsoft.VisualBasic.Imaging.Math2D
 Imports Microsoft.VisualBasic.Imaging.Physics
+Imports Microsoft.VisualBasic.Math.Distributions
 
 Public Module Math
 
+    <Extension>
     Public Function MoranI(cells As IEnumerable(Of CellScan), Optional knn As Integer = 16) As CellScan()
         Dim all As CellScan() = cells.ToArray
         Dim averageR As Double = Aggregate cell As CellScan In all Into Average((cell.width + cell.height) / 2)
@@ -15,8 +19,32 @@ Public Module Math
                 .OrderBy(Function(a) target.DistanceTo(a)) _
                 .Take(knn) _
                 .ToArray
+            Dim data = nearby.Select(Function(a) a.ratio).ToArray
+            Dim c1 = nearby.X
+            Dim c2 = nearby.Y
+            Dim moranVal = Moran.calc_moran(data, c1, c2)
+            Dim pv As Double = pnorm.eval(moranVal.observed,
+                                     mean:=moranVal.expected,
+                                     sd:=moranVal.sd,
+                                     resolution:=1000)
 
+            If moranVal.observed <= -1 / (data.Length - 1) Then
+                pv = 2 * pv
+            Else
+                pv = 2 * (1 - pv)
+            End If
+
+            If pv < 0 Then
+                pv = 1 / Single.MaxValue
+            ElseIf pv.IsNaNImaginary Then
+                pv = 1
+            End If
+
+            target.moranI = moranVal.observed
+            target.pvalue = pv
         Next
+
+        Return all
     End Function
 
 End Module
