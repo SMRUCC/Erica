ca139fbc4ee46579cacf5a3a85fa3bd2b7d8e9a9
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-09-01 13:21:24 +0800

    calculate the size

--- src/HEView/DziImage.vb
+++ src/HEView/DziImage.vb
@@ -32,35 +32,27 @@ Public Class DziImage
     ''' <param name="row">行号 (从0开始)</param>
     ''' <returns>表示瓦片位置和大小的矩形</returns>
     ''' <remarks>
-    ''' ​图像金字塔与层级（Level）​​
+    ''' 1. 图像金字塔与层级（Level）
     ''' 
-    ''' DZI将高分辨率图像处理为一系列层级（Level）的金字塔结构。​层级0（Level 0）是分辨率最高的原始图像。每增加一个层级，图像的宽度和高度就缩减为上一层级的一半（采用Ceiling向上取整），直至最终变为1x1像素
-    ''' 。
+    ''' DZI将高分辨率图像处理为一系列层级（Level）的金字塔结构。层级0（Level 0）是分辨率最高的原始图像。每增加一个层级，
+    ''' 图像的宽度和高度就缩减为上一层级的一半（采用Ceiling向上取整），直至最终变为1x1像素 。
     ''' 
-    ''' ​最大层级的计算公式为：MaxLevel = Ceiling(Log2(Max(Width, Height)))
-    ''' 。这意味着对于一张 4224x3168 的图片，其最大层级为 13。
+    ''' 最大层级的计算公式为：MaxLevel = Ceiling(Log2(Max(Width, Height)))。这意味着对于一张 4224x3168 的图片，其最大层级为 13。
     ''' 
-    ''' 2.
-    ''' ​瓦片（Tile）网格​
+    ''' 2. 瓦片（Tile）网格
     ''' 
     ''' 在每个层级上，图像会被分割成多个尺寸为 TileSize x TileSize的瓦片（除了最右边和最下边的瓦片，它们可能因为不能整除而更小）。
+    ''' 每个层级瓦片的列数（Columns）和行数（Rows）计算公式为：
     ''' 
-    ''' ​每个层级瓦片的列数（Columns）和行数（Rows）​计算公式为：
+    ''' + Columns = Ceiling(LevelWidth / TileSize)
+    ''' + Rows = Ceiling(LevelHeight / TileSize)
     ''' 
-    ''' •
-    ''' Columns = Ceiling(LevelWidth / TileSize)
+    ''' 3.瓦片重叠（Overlap）
     ''' 
-    ''' •
-    ''' Rows = Ceiling(LevelHeight / TileSize)
-    ''' 
-    ''' 3.
-    ''' ​瓦片重叠（Overlap）​​
-    ''' 
-    ''' DZI 允许设置 Overlap（通常为1像素）。​重叠是指相邻瓦片之间会重复一部分像素区域。这样做主要是为了在图像拼接渲染时消除瓦片间的缝隙或瑕疵，确保视觉上的连续性
-    ''' 。
-    ''' 
-    ''' 计算瓦片位置和尺寸时，​非边缘的瓦片会向四个方向延伸 Overlap个像素。位于图像边缘（左上、右上、左下、右下、上边界、下边界、左边界、右边界）的瓦片，其重叠区域需要特殊处理，不能超出图像的实际边界
-    ''' 。
+    ''' DZI 允许设置 Overlap（通常为1像素）。重叠是指相邻瓦片之间会重复一部分像素区域。这样做主要是为了在图像拼接渲染时消除瓦片
+    ''' 间的缝隙或瑕疵，确保视觉上的连续性。
+    ''' 计算瓦片位置和尺寸时，非边缘的瓦片会向四个方向延伸 Overlap个像素。位于图像边缘（左上、右上、左下、右下、上边界、下边界、
+    ''' 左边界、右边界）的瓦片，其重叠区域需要特殊处理，不能超出图像的实际边界。
     ''' </remarks>
     Public Function DecodeTile(level As Integer, col As Integer, row As Integer) As Rectangle
         ' 1. 计算当前层级的图像尺寸
