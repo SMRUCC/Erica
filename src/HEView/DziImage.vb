bcba1fdb83c42ae6294d5c06b331590dc1d6db80
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-09-01 11:40:49 +0800

    export dir

---
+++ src/HEView/DziImage.vb
@@ -0,0 +1,30 @@
+﻿Imports System.Drawing
+Imports System.Xml.Serialization
+
+<XmlType("Image", [Namespace]:="http://schemas.microsoft.com/deepzoom/2008")>
+Public Class DziImage
+
+    <XmlAttribute> Public Property Format As String
+    <XmlAttribute> Public Property Overlap As Integer
+    <XmlAttribute> Public Property TileSize As Integer
+
+    Public Property Size As SizeInt
+
+    Public Class SizeInt
+        <XmlAttribute> Public Property Width As Integer
+        <XmlAttribute> Public Property Height As Integer
+
+        Public Overrides Function ToString() As String
+            Return $"{{width:{Width}, height:{Height}}}"
+        End Function
+    End Class
+
+    Public Overrides Function ToString() As String
+        Return Size.ToString
+    End Function
+
+    Public Function DecodeOffsets() As Point
+
+    End Function
+
+End Class
