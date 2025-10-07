5650cd7b63d146298406371e91e22bf491786634
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-10-07 23:44:14 +0800

    read TCGA dataset and make pre-processing

--- test/TCGA_dataset.R
+++ test/TCGA_dataset.R
@@ -1,6 +1,15 @@
 require(Erica);
 
 imports "singleCell" from "Erica";
+imports "geneExpression" from "phenotype_kit";
 
 let data = singleCell::read.TCGA_mtx("/marker/test/aa55ac5e-7a48-45fd-9fb5-7e013805b247/");
 
+data = data 
+|> geneExpression::load.expr() 
+|> geneExpression::tr()
+|> geneExpression::totalSumNorm()
+|> geneExpression::relative(median = TRUE)
+;
+
+write.expr_matrix(data, file = "/marker/test/aa55ac5e-7a48-45fd-9fb5-7e013805b247.csv");
