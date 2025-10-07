aa4c72e78bae20fa3c5d4844aa5dc7b59d918e4e
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-10-08 00:12:23 +0800

    Update TCGA_dataset.R

--- test/TCGA_dataset.R
+++ test/TCGA_dataset.R
@@ -8,8 +8,11 @@ let data = singleCell::read.TCGA_mtx("/marker/test/aa55ac5e-7a48-45fd-9fb5-7e013
 data = data 
 |> geneExpression::load.expr() 
 |> geneExpression::tr()
+|> geneExpression::impute_missing(by_features=TRUE)
 |> geneExpression::totalSumNorm()
 |> geneExpression::relative(median = TRUE)
+|> geneExpression::sort_mad(top = 5000)
+|> geneExpression::tr()
 ;
 
 write.expr_matrix(data, file = "/marker/test/aa55ac5e-7a48-45fd-9fb5-7e013805b247.csv");
