16d8ab0be3583596629721040ca2b93112cd9b39
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-10-14 23:19:53 +0800

    needs more memory for processing the scaning

--- src/HEView/DziScanner.vb
+++ src/HEView/DziScanner.vb
@@ -80,13 +80,9 @@ Public Module DziScanner
 
         Erase imagefiles
 
+        b = DziImageBuffer.GlobalScales(b)
         r = DziImageBuffer.GlobalScales(r)
         g = DziImageBuffer.GlobalScales(g)
-        b = DziImageBuffer.GlobalScales(b)
-
-        Call b.DumpExport("K:/IHC1/b/")
-        Call r.DumpExport("K:/IHC1/r/")
-        Call g.DumpExport("K:/IHC1/g/")
 
         Call "scan cells in red channel...".info
         Dim r_cells As CellScan() = r.ScanBuffer(ostu_factor:=ostu_factor, flip:=False, splitBlocks:=splitBlocks, noise:=noise, moran_knn:=moran_knn).ToArray
