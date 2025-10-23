e094572ed2a4571dddd0b007027c5b475c66acd7
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-10-23 13:04:34 +0800

    too much steps

--- test/HE/align_manually.R
+++ test/HE/align_manually.R
@@ -1,10 +1,13 @@
 require(geometry2D);
+require(Erica);
+
+imports "singleCell" from "Erica";
 
 let HE = read.csv("C:\Users\Administrator\Desktop\MLKL-5\HE_cells.csv", row.names = 1, check.names = FALSE);
 let CD31_PAS = read.cells("C:\Users\Administrator\Desktop\MLKL-5\CD31+PAS_cells.csv");
 let t = geometry2D::transform(theta = 0,
-                              translate = [0,0],
-                              scale = [0.85,1]);
+                              translate = [1260,-100],
+                              scale = [0.95,0.98]);
 
 CD31_PAS = CD31_PAS |> geo_transform(t) |> as.data.frame();
 
@@ -16,9 +19,11 @@ let cells_xy = data.frame(
         rep("CD31+PAS", times = nrow(CD31_PAS)))
 );
 
-bitmap(file = "C:\Users\Administrator\Desktop\MLKL-5\HE_overlaps_CD31+PAS.png") {
+bitmap(file = "C:\Users\Administrator\Desktop\MLKL-5\HE_overlaps_CD31+PAS.png", size = [2400 3000]) {
     plot(as.numeric(cells_xy$x), 
         as.numeric(cells_xy$y), 
-        class = cells_xy$class
+        class = cells_xy$class,
+        point_size= 3,
+        fill = "white"
     );
 }
