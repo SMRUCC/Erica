605211e8f596a6c83bdf6fd3db66629b2ba44c2f
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-10-24 15:45:34 +0800

    Update align_RANSAC.R

--- test/HE/align_RANSAC.R
+++ test/HE/align_RANSAC.R
@@ -6,8 +6,8 @@ let CD31_PAS = read.csv("C:\Users\Administrator\Desktop\MLKL-5\CD31+PAS_cells.cs
 
 print(HE, max.print = 13);
 
-HE = data.frame(x=HE$physical_x,y= HE$physical_y) |> concaveHull(as.polygon = TRUE);
-CD31_PAS = data.frame(x=CD31_PAS$physical_x,y= CD31_PAS$physical_y) |> concaveHull(as.polygon = TRUE);
+HE = polygon2D(HE$physical_x, HE$physical_y) |> rasterize() |> polygon2D(raster_n =3) |> concaveHull(as.polygon = TRUE);
+CD31_PAS = polygon2D(CD31_PAS$physical_x, CD31_PAS$physical_y) |> rasterize() |> polygon2D(raster_n =3) |> concaveHull(as.polygon = TRUE);
 
 print(HE);
 print(CD31_PAS);
