6dc8af6a91d488cb3ab1a7571da44ddca98283b5
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-10-26 16:15:40 +0800

    Update 2_main_shape.R

--- test/HE/RANSAC/2_main_shape.R
+++ test/HE/RANSAC/2_main_shape.R
@@ -10,22 +10,28 @@ let CD31_PAS = read.csv( file.path(dir, "CD31+PAS.csv") , row.names =NULL, check
 print(HE, max.print = 13);
 
 let get_mainshape = function(multishapes, name) {
-    multishapes = knn_cluster(multishapes, knn = 64, p = 0.8);
+    multishapes = knn_cluster(multishapes, knn =16, p = 0.5);
     multishapes = as.data.frame(multishapes);
 
     print(multishapes, max.print = 16);
 
+    let largest = table( multishapes$Cluster,desc=TRUE);
+
+    print(largest);
+
+    largest = multishapes[.Internal::first( largest$factors) ==multishapes$Cluster , ]; 
+
     # show object detection result
     bitmap(file = file.path(dir, `shapes_${name}.png`)) {
-        plot(as.numeric(multishapes[, "x"]), as.numeric(multishapes[, "y"]), 
-            class     = multishapes[, "Cluster"], 
+        plot(as.numeric(largest[, "x"]), as.numeric(largest[, "y"]), 
+            class     = largest[, "Cluster"], 
             grid.fill = "white",
             padding   = "padding: 125px 300px 200px 200px;",
             colorSet  = "paper"
         );
     }
 
-    write.csv(multishapes, file = file.path(dir,`shapes_${name}.csv` ));
+    write.csv(largest, file = file.path(dir,`shapes_${name}.csv` ));
 }
 
 get_mainshape(HE,"HE");
