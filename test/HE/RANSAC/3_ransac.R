2cdadb927deb0eb20a51c4813fb661397cebf49f
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-10-26 18:14:56 +0800

    Update 3_ransac.R

--- test/HE/RANSAC/3_ransac.R
+++ test/HE/RANSAC/3_ransac.R
@@ -3,13 +3,13 @@ imports "machineVision" from "signalKit";
 
 let dir = "C:\Users\Administrator\Desktop\MLKL-5\raster";
 
-let HE = read.csv(file.path(dir, "shapes_HE.csv"), row.names = 1, check.names = FALSE);
-let CD31_PAS = read.csv(file.path(dir,"shapes_CD31+PAS.csv"), row.names = 1, check.names = FALSE);
+let HE = read.csv(file.path(dir, "HE.csv"), row.names = NULL, check.names = FALSE);
+let CD31_PAS = read.csv(file.path(dir,"CD31+PAS.csv"), row.names = NULL, check.names = FALSE);
 
 HE = polygon2D(as.numeric(HE$x), as.numeric(HE$y));
 CD31_PAS = polygon2D(as.numeric(CD31_PAS$x), as.numeric(CD31_PAS$y));
 
-let t = RANSAC(CD31_PAS, HE);
+let t = RANSAC(CD31_PAS, HE, iterations= 100000, threshold  = 0.05);
 
 str(t);
 
@@ -28,9 +28,9 @@ let un_aligned = rbind(HE, CD31_PAS);
 write.csv(aligned, file = file.path(dir, "RANSAC", "aligned.csv"), row.names = FALSE);
 
 bitmap(file = file.path(dir, "RANSAC", "unaligned.png")) {
-    plot(as.numeric(un_aligned$x),as.numeric(un_aligned$y), class = un_aligned$class, fill = "white",point_size= 1);
+    plot(as.numeric(un_aligned$x),as.numeric(un_aligned$y), class = un_aligned$class, fill = "white",point_size= 3, colors = "paper");
 }
 
 bitmap(file = file.path(dir, "RANSAC", "aligned.png")) {
-    plot(as.numeric(aligned$x),as.numeric(aligned$y), class = aligned$class, fill = "white",point_size= 1);
+    plot(as.numeric(aligned$x),as.numeric(aligned$y), class = aligned$class, fill = "white",point_size= 3, colors = "paper");
 }
