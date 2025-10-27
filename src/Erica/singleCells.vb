6f8422cff4ea44c955e4318a15378dabdef26e4f
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-10-27 21:23:31 +0800

    add missing rotation angle value

--- src/Erica/singleCells.vb
+++ src/Erica/singleCells.vb
@@ -111,6 +111,7 @@ Public Module singleCells
         Call df.add("size", From cell As CellScan In cells Select cell.points)
         Call df.add("r1", From cell As CellScan In cells Select cell.r1)
         Call df.add("r2", From cell As CellScan In cells Select cell.r2)
+        Call df.add("theta", From cell As CellScan In cells Select cell.theta)
         Call df.add("weight", From cell As CellScan In cells Select cell.weight)
         Call df.add("density", From cell As CellScan In cells Select cell.density)
         Call df.add("mean_distance", From cell As CellScan In cells Select cell.average_dist)
