3d2f52a01e77d3a07b77c11b9b3b66ab7ec7c41f
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-10-29 16:35:54 +0800

    Update RANSAC_cells.R

--- test/HE/RANSAC_cells.R
+++ test/HE/RANSAC_cells.R
@@ -5,6 +5,22 @@ imports "singleCell" from "Erica";
 
 let MSI = read.cells("C:\Users\Administrator\Desktop\MLKL-5\MSI_cells.csv");
 let HE = read.cells("C:\Users\Administrator\Desktop\MLKL-5\HE_cells.csv");
-let t = singleCell::RANSAC_cell_alignment(HE, MSI, distance_threshold = 100);
+let t = singleCell::RANSAC_cell_alignment(HE, MSI, distance_threshold = 10);
 
 print(t);
+
+HE = HE |> geo_transform(t);
+HE = as.data.frame(HE);
+HE[,"class"] = "HE";
+
+MSI = as.data.frame(MSI);
+MSI[,"class"] = "MSImaging";
+
+let cells = rbind(HE, MSI);
+
+bitmap(file = "C:\Users\Administrator\Desktop\MLKL-5_MSI+HE_cellWeights.png") {
+    plot(as.numeric(cells$physical_x), as.numeric(cells$physical_y), 
+        point_size = 5, 
+        class = cells$class, 
+        fill = "white");
+}
