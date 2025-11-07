051a964bf5d2332a688d4a5acb526b7e800d2c8f
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-11-07 09:51:12 +0800

    Update RANSAC_cells.R

--- scripts/RANSAC_cells.R
+++ scripts/RANSAC_cells.R
@@ -3,8 +3,8 @@ require(Erica);
 
 imports "singleCell" from "Erica";
 
-let target = ?"--target" || stop("");
-let reference = ?"--subject" || stop("");
+let target = ?"--target" || stop("missing target slice for make the RANSAC alignment!");
+let reference = ?"--subject" || stop("missing the reference slice for the target alignment.");
 let grids = ?"--grid" ||  900;
 let output_dir = ?"--output" || file.path(dirname(target), `${basename(target)}_RANSAC_cell`);
 
@@ -17,6 +17,8 @@ let t = singleCell::RANSAC_cell_alignment(target_raster, subject_raster, distanc
 
 print(t);
 
+let unaligned = as.data.frame(target_slice);
+
 target_slice = target_slice |> geo_transform(t);
 target_slice = as.data.frame(target_slice);
 target_slice[,"class"] = basename(target);
@@ -25,10 +27,21 @@ subject_slice [,"class"] = basename(reference);
 
 let cells = rbind(target_slice, subject_slice);
 
+unaligned[,"class"] = `${basename(target)}_unaligned`;
+unaligned = rbind(unaligned, subject_slice);
+
 bitmap(file = file.path(output_dir,"RANSAC_cell.png"), size = [6000,4800]) {
     plot(as.numeric(cells$physical_x), as.numeric(cells$physical_y), 
         point_size = 2, 
         class = cells$class, 
+        colors = "paper",
+        fill = "white");
+}
+
+bitmap(file = file.path(output_dir,"unaligned_cell.png"), size = [6000,4800]) {
+    plot(as.numeric(unaligned$physical_x), as.numeric(unaligned$physical_y), 
+        point_size = 2, 
+        class = unaligned$class, 
         colors = "paper",
         fill = "white");
 }
