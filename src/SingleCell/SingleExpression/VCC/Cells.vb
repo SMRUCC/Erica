99691fda2d239cd685bf269fe07d62429f8d826c
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-08-21 14:48:58 +0800

    create table export model

---
+++ src/SingleCell/SingleExpression/VCC/Cells.vb
@@ -0,0 +1,15 @@
+﻿Public Class Cells
+
+    Public Property cell_id As String
+    Public Property target_gene As String
+    Public Property context As String
+
+End Class
+
+Public Class Counts
+
+    Public Property cell_id As String
+    Public Property gene_name As String
+    Public Property count As UInteger
+
+End Class
